0e8213da3e710620d528d23b22d252a4aff6eb50
Rudi Bruchez <rudi@babaluga.com>
2023-08-31 14:15:25 +0200

    premier commit

---
+++ SqlApi/Program.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using Microsoft.Azure.Cosmos;
+using Azure.Identity;
+using System.Threading.Tasks;
+
+namespace SqlApi
+{
+    public class Inscription 
+    {
+        public string id { get; set; }
+        public int InscriptionId { get; set; }
+        public string Prenom { get; set; }
+        public string Nom { get; set; }
+        public string Email { get; set; }
+        public string Adresse1 { get; set; }
+        public string CodePostal { get; set; }
+        public string Ville { get; set; }
+        public string Titre { get; set; }
+        public DateTime DateDebut { get; set; }
+    }
+
+    class Program
+    {
+        private static async Task Select(string nom, Container ins)
+        {
+            var query = new QueryDefinition(
+                query: "SELECT * FROM Inscriptions i WHERE i.Nom = @nom"
+                )
+                .WithParameter("@nom", "Feragotto");
+
+            using FeedIterator<Inscription> feed = ins.GetItemQueryIterator<Inscription>(queryDefinition: query);
+
+            while (feed.HasMoreResults)
+            {
+                FeedResponse<Inscription> response = await feed.ReadNextAsync();
+                foreach (Inscription item in response)
+                {
+                    Console.WriteLine($"Found item:\t{item.Email}");
+                }
+            }
+        }
+
+        private static async Task Update(string nom, string email, Container ins)
+        {
+            PatchItemRequestOptions options = new()
+            {
+                ConsistencyLevel = ConsistencyLevel.Eventual
+            };
+
+            var query = new QueryDefinition(
+                query: "SELECT * FROM Inscriptions i WHERE i.Nom = @nom"
+                )
+                .WithParameter("@nom", "Feragotto");
+
+            using var feed = ins.GetItemQueryIterator<Inscription>(queryDefinition: query);
+
+            if (feed.HasMoreResults)
+            {
+                FeedResponse<Inscription> response = await feed.ReadNextAsync();
+
+                var item = response.FirstOrDefault();
+
+                List<PatchOperation> operations = new ()
+                {
+                    PatchOperation.Replace($"/Email", email),
+                };
+
+                ItemResponse<Inscription> reponse = await ins.PatchItemAsync<Inscription>(
+                    id: item.id,
+                    partitionKey: new PartitionKey(item.Titre),
+                    patchOperations: operations,
+                    requestOptions: options
+                );
+
+                Console.WriteLine($"Resultat :\t{reponse.StatusCode}");
+
+            }            
+        }
+
+        private static async Task ReplaceEtag(string nom, Container ins)
+        {
+            var query = new QueryDefinition(
+                query: "SELECT * FROM Inscriptions i WHERE i.Nom = @nom"
+                )
+                .WithParameter("@nom", "Feragotto");
+
+            using FeedIterator<Inscription> feed = ins.GetItemQueryIterator<Inscription>(queryDefinition: query);
+
+            if (feed.HasMoreResults)
+            {
+                FeedResponse<Inscription> response = await feed.ReadNextAsync();
+
+                var item = response.FirstOrDefault();
+                var requestOptions = new ItemRequestOptions { IfMatchEtag = response.ETag };
+
+                var rep2 = await ins.UpsertItemAsync<Inscription>(item, requestOptions: requestOptions);
+                
+                await Console.Out.WriteLineAsync($"Nouveau ETag:\t{rep2.ETag}");
+            }
+        }
+
+
+        static async Task Main(string[] args)
+        {
+            CosmosClientOptions clientOptions = new CosmosClientOptions()
+            {
+                ApplicationPreferredRegions = new List<string>(){ 
+                    Regions.WestEurope, 
+                    Regions.NorthEurope
+                }
+            };
+            // New instance of CosmosClient class
+            using CosmosClient client = new(
+                accountEndpoint: "https://cosmospachadata.documents.azure.com:443/",
+                tokenCredential: new DefaultAzureCredential(),
+                clientOptions: clientOptions    
+            );
+
+            Database db = client.GetDatabase(id: "pachadata");
+            Container ins = db.GetContainer(id: "Inscriptions");
+
+            await Update("Feragotto", "feragotto@ici.fr", ins);
+
+        }
+   }
+}
---
+++ TableApi/Program.cs
@@ -0,0 +1,80 @@
+﻿// dotnet add package Azure.Data.Tables
+using Azure;
+using Azure.Data.Tables;
+using System.Text.Json;
+
+namespace SqlApi
+{
+    public class Inscription: ITableEntity 
+    {
+        public string RowKey { get; set; } = default!;
+        public string PartitionKey { get; set; } = default!;
+        public required int InscriptionId { get; set; }
+        public string? Prenom { get; set; }
+        public required string Nom { get; set; }
+        public string? Email { get; set; }
+        public string? Adresse1 { get; set; }
+        public string? CodePostal { get; set; }
+        public string? Ville { get; set; }
+        public required string Titre { get; set; }
+        public DateTime DateDebut { get; set; }
+        public ETag ETag { get; set; } = default!;
+        public DateTimeOffset? Timestamp { get; set; } = default!;
+    }
+
+    class Program
+    {
+        static async IAsyncEnumerable<Inscription?> GetInscriptions()
+        {
+            string fileName = "Inscriptions6000.json";
+            using FileStream openStream = File.OpenRead(fileName);
+            await foreach (var i in JsonSerializer.DeserializeAsyncEnumerable<Inscription>(openStream))
+            {
+                yield return i;
+            }
+            
+        }
+
+        static async Task SetInscriptions(TableClient tableClient)
+        {
+           await foreach (var i in GetInscriptions())
+            {
+                i.RowKey = i.InscriptionId.ToString();
+                i.PartitionKey = i.Titre;
+                i.DateDebut = DateTime.SpecifyKind(i.DateDebut, DateTimeKind.Utc);
+                
+                await tableClient.AddEntityAsync<Inscription>(i);
+            }            
+        }
+
+        static async Task GetInscription(TableClient tableClient, string titre, int inscriptionId)
+        {
+            var i = await tableClient.GetEntityIfExistsAsync<Inscription>(titre, inscriptionId.ToString());
+
+            Console.WriteLine($"Inscrit : {i.Value.Email}");        
+        }
+
+
+        static async Task Main(string[] args)
+        {
+
+            // New instance of the TableClient class
+            TableServiceClient tableServiceClient = new TableServiceClient(
+                    "DefaultEndpointsProtocol=https;AccountName=pachadata-kv;AccountKey=7Ct61YximHSrSNILcNcu7yPFKIlw4ZHWnWuh4hlJTQC7AnLQqqFwaJMAeK9WTjOHuQhl1bYGi06tACDbgevo5g==;TableEndpoint=https://pachadata-kv.table.cosmos.azure.com:443/"
+            );
+
+            TableClient tableClient = tableServiceClient.GetTableClient(
+                tableName: "inscriptions"
+            );
+
+            await tableClient.CreateIfNotExistsAsync();
+
+            // écrire
+            //await SetInscriptions(tableClient);
+
+            // lire
+            await GetInscription(tableClient, "Firebird 2.0 Administration", 10004);
+
+        }
+    }
+}
